0397ffeac804ca2f25730aaeb85a20eade1effee
Mika Ridley <mika.ridley26@gmail.com>
2025-10-26 21:19:36 +0200

    Add files via upload

---
+++ img.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const MINE = '<img src="img/Cell - Mine.png" alt="mine" />'
+const MINE_RED = '<img src="img/Cell - Red Mine.png" alt="mine" />'
+const COVER = '<img src="img/Cell - Unrevealed.png" alt="cover" />'
+const FLAG = '<img src="img/Cell - Flag.png" alt="flag" />'
+const HEART = '<img src="img/Heart - Full.png" alt="heart" />'
+const EMPTY_HEART = '<img src="img/Heart - Empty.png" alt="empty heart" />'
+const EASY_BTN = "url('img/Easy Button.png')"
+const EASY_BTN_CLICKED = "url('img/Easy Button - Selected.png')"
+const MEDIUM_BTN = "url('img/Medium Button.png')"
+const MEDIUM_BTN_CLICKED = "url('img/Medium Button - Selected.png')"
+const HARD_BTN = "url('img/Hard Button.png')"
+const HARD_BTN_CLICKED = "url('img/Hard Button - Selected.png')"
+const DARK_MODE = '<img src="img/Mode - Dark.png" alt="dark mode" />'
+const LIGHT_MODE = '<img src="img/Mode - Light.png" alt="light mode" />'
+const HINTS_ON = '<img src="img/Hints Button - On.png" alt="hints-on" />'
+const HINTS_OFF = '<img src="img/Hints Button - Off.png" alt="hints-off" />'
+const HINTS_USED = '<img src="img/Hints Button - Used.png" alt="hints-used" />'
+const SAFE_CELL = '<img src="img/Safe Click Button.png" alt="safe cell-used" />'
+const SAFE_CELL_USED =
+  '<img src="img/Safe Click Button - Used.png" alt="safe cell-used" />'
+const YELLOW_CELL = '<img src="img/Safe Cell.png" alt="yellow cell" />'
+const UNDO = '<img src="img/Undo Button.png" alt="undo-used" />'
+const UNDO_USED = '<img src="img/Undo Button - Used.png" alt="undo-used" />'
+const ETIDOR_X = '<img src="img/Editor Mode - X.png" alt="editor-x" />'
+const ETIDOR_V = '<img src="img/Editor Mode - V.png" alt="editor-y" />'
+const MEGA = '<img src="img/Mega Hint Button.png" alt="mega" />'
+const MEGA_ON = '<img src="img/Mega Hint Button - On.png" alt="mega-on" />'
+const MEGA_USED =
+  '<img src="img/Mega Hint Button - Used.png" alt="mega-used" />'
+const EXT = '<img src="img/Exterminator Button.png" alt="Exterminator" />'
+const EXT_USED =
+  '<img src="img/Exterminator Button - Used.png" alt="Exterminator used" />'
